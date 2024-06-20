c9b4dea734ae892c7ddac1a39240e9178645654f
bluesky-dev12 <ravello17@gmail.com>
2024-06-21 00:05:00 +0100

    Adding compiler and decompiler.

---
+++ Compiler/Compile_EXAMPLE.bat
@@ -0,0 +1,5 @@
+fxc /T fx_2_0 carbon/visualtreatment.fx /Fo D:/NFSUR/NFSUR/IDI_visualtreatment_FX
+fxc /T fx_2_0 carbon/particles.fx /Fo D:/NFSUR/NFSUR/IDI_PARTICLES_FX
+fxc /T fx_2_0 carbon/car.fx /Fo D:/NFSUR/NFSUR/IDI_CAR_FX
+
+echo Done.
